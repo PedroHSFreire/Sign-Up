e1ad3f020308e521d6e956ae77a090304ac2972c
Pedro <psilvafreire@gmail.com>
2023-07-24 16:46:06 -0300

    first commit

---
+++ script/script.js
@@ -0,0 +1,8 @@
+const login = document.getElementById("login");
+const pass = document.getElementById("password");
+const email = document.getElementById("email");
+const res = document.getElementById("teste");
+
+login.array.forEach((login) => {
+  login.addEventListener("click", () => {});
+});
